b58329b73d4dd3bbdf42daacfb8ff3f99d5a0213
Marko Djordjevic <marko@droopia.net>
2021-06-24 12:47:19 +0200

    starting front controller

--- index.php
+++ index.php
@@ -9,6 +9,17 @@ $request = Request::createFromGlobals();
 
 $name = $request->get('name', 'World');
 
+
+
+if ($myIp === $request->getClientIp()) {
+  // the client is a known one, so give it some more privilege
+}
+
+
+
 $response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
 
 $response->send();
+
+$response = new Response();
+
