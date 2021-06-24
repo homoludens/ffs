e4462b079e8bb03926547b679ed66096baa38e2f
Marko Djordjevic <marko@droopia.net>
2021-06-24 13:24:33 +0200

    a bit more routing

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,5 +1,2 @@
 <!-- example.com/src/pages/hello.php -->
-
-<?php $name = $request->get('name', 'World') ?>
-
-Hello <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
+Hello <?= htmlspecialchars(isset($name) ? $name : 'World', ENT_QUOTES, 'UTF-8') ?>
