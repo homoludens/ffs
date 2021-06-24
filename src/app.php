0f0547c820360ee7c20a434523b07c5345e00d66
Marko Djordjevic <marko@droopia.net>
2021-06-24 15:05:47 +0200

    adding more routing for controlers and template variables

--- src/app.php
+++ src/app.php
@@ -4,7 +4,12 @@
 use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', ['name' => 'World']));
+
+$routes->add('hello', new Routing\Route('/hello/{name}', [
+  'name' => 'World',
+  '_controller' => 'render_template',
+]));
+
 $routes->add('bye', new Routing\Route('/bye'));
 
 return $routes;
--- web/front.php
+++ web/front.php
@@ -1,33 +1,30 @@
 <?php
-
 // example.com/web/front.php
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
-use Symfony\Component\Routing\Matcher\CompiledUrlMatcher;
-use Symfony\Component\Routing\Matcher\Dumper\CompiledUrlMatcherDumper;
+
+function render_template($request)
+{
+  extract($request->attributes->all(), EXTR_SKIP);
+  ob_start();
+  include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+  return new Response(ob_get_clean());
+}
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
-// $compiledRoutes is a plain PHP array that describes all routes in a performant data format
-// you can (and should) cache it, typically by exporting it to a PHP file
-$compiledRoutes = (new CompiledUrlMatcherDumper($routes))->getCompiledRoutes();
-$matcher = new CompiledUrlMatcher($compiledRoutes, $context);
-
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
-
-  extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-  ob_start();
-  include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-  $response = new Response(ob_get_clean());
-
+  $request->attributes->add($matcher->match($request->getPathInfo()));
+  $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $exception) {
   $response = new Response('Not Found', 404);
 } catch (Exception $exception) {
@@ -35,3 +32,4 @@ try {
 }
 
 $response->send();
+
