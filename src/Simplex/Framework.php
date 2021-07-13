77efc784613733122e9b22fe0bfa09e06a9a442a
Marko Djordjevic <marko@droopia.net>
2021-07-13 21:19:40 +0200

    The DependencyInjection Component 1

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -13,9 +13,30 @@ use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
-use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpKernel;
 
-class Framework extends HttpKernel
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing;
+
+class Framework extends HttpKernel\HttpKernel
 {
+  public function __construct($routes)
+  {
+    $context = new Routing\RequestContext();
+    $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+    $requestStack = new RequestStack();
 
+    $controllerResolver = new HttpKernel\Controller\ControllerResolver();
+    $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+
+    $dispatcher = new EventDispatcher();
+    $dispatcher->addSubscriber(new HttpKernel\EventListener\ErrorListener(
+      'Calendar\Controller\ErrorController::exception'
+    ));
+    $dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
+    $dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
+    $dispatcher->addSubscriber(new StringResponseListener());
+
+    parent::__construct($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+  }
 }
--- web/front.php
+++ web/front.php
@@ -2,49 +2,20 @@
 // example.com/web/front.php
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Symfony\Component\EventDispatcher\EventDispatcher;
+
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\Routing;
+
+
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+// example.com/web/front.php
+require_once __DIR__.'/../vendor/autoload.php';
+
 
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
 $routes = include __DIR__.'/../src/app_year.php';
 
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+$framework = new Simplex\Framework($routes);
 
-$controllerResolver = new HttpKernel\Controller\ControllerResolver();
-$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
-
-$framework = new Simplex\Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
-
-// error management
-//$errorHandler = function (Symfony\Component\ErrorHandler\Exception\FlattenException $exception) {
-//  $msg = 'Something went wrong! ('.$exception->getMessage().')';
-//
-//  return new Response($msg, $exception->getStatusCode());
-//};
-//$dispatcher->addSubscriber(new HttpKernel\EventListener\ErrorListener($errorHandler));
-
-//
-$listener = new HttpKernel\EventListener\ErrorListener(
-  'Calendar\Controller\ErrorController::exception'
-);
-$dispatcher->addSubscriber($listener);
-
-//Response::prepare() method, which ensures that a Response is compliant with the HTTP specification
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-
-//streamed responses
-$dispatcher->addSubscriber(new HttpKernel\EventListener\StreamedResponseListener());
-
-$dispatcher->addSubscriber(new Simplex\StringResponseListener());
-
-$response = $framework->handle($request);
-$response->send();
+$framework->handle($request)->send();
