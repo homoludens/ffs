96bb8fb3b9486bbbe352a5dd8298e275ef5ae8dc
Marko Djordjevic <marko@droopia.net>
2021-07-13 17:42:53 +0200

    The EventDispatcher Component 1

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -3,30 +3,31 @@
 // example.com/src/Simplex/Framework.php
 namespace Simplex;
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-
-use Calendar\Controller\LeapYearController;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 
-class Framework {
-
+class Framework
+{
+  private $dispatcher;
   private $matcher;
-
   private $controllerResolver;
-
   private $argumentResolver;
 
-  public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $controllerResolver, ArgumentResolverInterface $argumentResolver) {
+  public function __construct(EventDispatcher $dispatcher, UrlMatcherInterface $matcher, ControllerResolverInterface $controllerResolver, ArgumentResolverInterface $argumentResolver)
+  {
+    $this->dispatcher = $dispatcher;
     $this->matcher = $matcher;
     $this->controllerResolver = $controllerResolver;
     $this->argumentResolver = $argumentResolver;
   }
 
-  public function handle(Request $request) {
+  public function handle(Request $request)
+  {
     $this->matcher->getContext()->fromRequest($request);
 
     try {
@@ -35,12 +36,16 @@ class Framework {
       $controller = $this->controllerResolver->getController($request);
       $arguments = $this->argumentResolver->getArguments($request, $controller);
 
-      return call_user_func_array($controller, $arguments);
+      $response = call_user_func_array($controller, $arguments);
     } catch (ResourceNotFoundException $exception) {
-      return new Response('Not Found', 404);
+      $response = new Response('Not Found', 404);
     } catch (\Exception $exception) {
-      return new Response('An error occurred', 500);
+      $response = new Response('An error occurred', 500);
     }
-  }
 
+    // dispatch a response event
+    $this->dispatcher->dispatch(new ResponseEvent($response, $request), 'response');
+
+    return $response;
+  }
 }
--- web/front.php
+++ web/front.php
@@ -9,6 +9,8 @@ use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
 //use Simplex\Framework;
 
 function render_template(Request $request)
@@ -28,11 +30,33 @@ $routes = include __DIR__.'/../src/app_year.php';
 $context = new Routing\RequestContext();
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
+$dispatcher = new EventDispatcher();
+$dispatcher->addListener('response', function (Simplex\ResponseEvent $event) {
+  $response = $event->getResponse();
+
+  if ($response->isRedirection()
+    || ($response->headers->has('Content-Type') && false === strpos($response->headers->get('Content-Type'), 'html'))
+    || 'html' !== $event->getRequest()->getRequestFormat()
+  ) {
+    return;
+  }
+
+  $response->setContent($response->getContent().'GA CODE');
+});
+
+$dispatcher->addListener('response', function (Simplex\ResponseEvent $event) {
+  $response = $event->getResponse();
+  $headers = $response->headers;
+
+  if (!$headers->has('Content-Length') && !$headers->has('Transfer-Encoding')) {
+    $headers->set('Content-Length', strlen($response->getContent()));
+  }
+}, -255);
+
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
-$framework = new Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
+$framework = new Simplex\Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
 $response = $framework->handle($request);
 
 $response->send();
-
